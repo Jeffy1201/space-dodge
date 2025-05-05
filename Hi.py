4ff5004840e3b257f1fc4c5075cd0c14a6629098
Jeffy1201 <jeffy1201hmm@gmail.com>
2025-05-05 12:52:19 +0930

    Made new game

---
+++ Hi.py
@@ -0,0 +1,31 @@
+Press = "Press Enter Please"
+Mean = "you are being realy mean"
+Listening = "I told you to Listen and you ignorn me"
+YouAreGoodBoyorgirl = "Good Boy/Girl"
+Hi = "Hello"
+Speaking = "So Now that you do what I say ill say Press enter"
+Hehe = "..."
+why = "why dont you become my living chair if you keep doing what i say"
+whY = "it's pontless to do anything else,"
+wHY = "if you did you would become dead"
+Why = "Mabey I could just sell you to make some money off of you"
+pointless = "you pointless person."
+
+print (Hi)
+print (Listening)
+print (Mean)
+print (Press)
+input()
+print (YouAreGoodBoyorgirl)
+print (Speaking)
+input()
+print (Hehe)
+print (why)
+print (whY)
+print (wHY)
+print (Press)
+input()
+print (Why)
+print (pointless)
+print (Press)
+input()
