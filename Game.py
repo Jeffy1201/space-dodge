9bd4952e2f5b37336cf5256368b4746a2c586126
Jeffy1201 <jeffy1201hmm@gmail.com>
2025-05-07 12:33:44 +0930

    more money

--- Game.py
+++ Game.py
@@ -2,7 +2,7 @@ import random
 
 wining = "You won a tikiet to get lost of money at random"
 press = "press Enter to see what you got"
-random_integer = random.randint(1, 10000)
+random_integer = random.randint(1, 1000000)
 
 print (wining)
 print (press)
